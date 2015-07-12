f29f3b7c73cd50ea3201e174b84451a1afd48ee7
Eugene Manuilov <eugene.manuilov@get10up.com>
2015-07-12 15:33:44 +0300

    Added test which checks downsize generation for not existing image size.

--- tests/functional/DownsizeCest.php
+++ tests/functional/DownsizeCest.php
@@ -19,6 +19,22 @@ class DownsizeCest {
 	protected $_attachment;
 
 	/**
+	 * The image filename.
+	 *
+	 * @access protected
+	 * @var string
+	 */
+	protected $_filename;
+
+	/**
+	 * The image url.
+	 *
+	 * @access protected
+	 * @var string
+	 */
+	protected $_url;
+
+	/**
 	 * Creates a new attachment for a test.
 	 *
 	 * @access public
@@ -29,10 +45,20 @@ class DownsizeCest {
 			$this->_factory = \WPCC\Helper\Factory::create();
 		}
 
-		$tmp_name = wp_tempnam( 'image' );
-		$this->_attachment = $this->_factory->attachment->createAndGet( array(
-			'file' => $tmp_name,
-		) );
+		$dirs = wp_upload_dir();
+		if ( ! is_dir( $dirs['path'] ) ) {
+			mkdir( $dirs['path'] );
+		}
+
+		$baseimage = codecept_data_dir() . 'image.jpg';
+		$filename = wp_unique_filename( $dirs['path'], 'downsize-image-test.jpg' );
+
+		$this->_url = trailingslashit( $dirs['url'] ) . $filename;
+		$this->_filename = rtrim( $dirs['path'], DIRECTORY_SEPARATOR ) . DIRECTORY_SEPARATOR . $filename;
+
+		copy( $baseimage, $this->_filename );
+
+		$this->_attachment = $this->_factory->attachment->createAndGet( array( 'file' => $this->_filename ) );
 	}
 
 	/**
@@ -45,9 +71,16 @@ class DownsizeCest {
 		$this->_factory->cleanup();
 	}
 
-	// tests
-	public function tryToTest( FunctionalTester $I ) {
-
+	/**
+	 * Checks if downsize returns url to a full size image if not existing image size is passed.
+	 *
+	 * @access public
+	 * @param FunctionalTester $I The tester instance.
+	 */
+	public function testNotExistingImageSize( FunctionalTester $I ) {
+		$image = wp_get_attachment_image_src( $this->_attachment->ID, 'non-existing-image-size' . rand( 0, 100 ) );
+		$I->assertNotEmpty( $image );
+		$I->assertEquals( $this->_url, current( $image ) );
 	}
 
 }
