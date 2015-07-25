1ca8addebd58a267d73f15228f5ae47b8c470e51
Eugene Manuilov <eugene.manuilov@get10up.com>
2015-07-25 13:20:28 +0300

    Added license blocks.

--- image-generator.php
+++ image-generator.php
@@ -9,6 +9,24 @@
  * License: GPL version 2 or later - http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
  */
 
+// +----------------------------------------------------------------------+
+// | Copyright 2015 10up Inc                                              |
+// +----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify |
+// | it under the terms of the GNU General Public License, version 2, as  |
+// | published by the Free Software Foundation.                           |
+// |                                                                      |
+// | This program is distributed in the hope that it will be useful,      |
+// | but WITHOUT ANY WARRANTY; without even the implied warranty of       |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        |
+// | GNU General Public License for more details.                         |
+// |                                                                      |
+// | You should have received a copy of the GNU General Public License    |
+// | along with this program; if not, write to the Free Software          |
+// | Foundation, Inc., 51 Franklin St, Fifth Floor, Boston,               |
+// | MA 02110-1301 USA                                                    |
+// +----------------------------------------------------------------------+
+
 // do nothing if PHP version is less than 5.5
 if ( version_compare( PHP_VERSION, '5.5', '<' ) ) {
 	return;
